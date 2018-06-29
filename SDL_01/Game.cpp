23642116494e8e917e630e50401fc60d094abebb
Francisco Bahamondes <francisco.bahamondes@hipodromo.local>
2018-06-29 16:15:39 -0400

    asdasdasd

--- SDL_01/Game.cpp
+++ SDL_01/Game.cpp
@@ -1,4 +1,4 @@
-#include "Game.cpp"
+#include "Game.h"
 
 bool Game::init(const char* title, int xpos, int ypos, int width, int height, int flags)
 {
@@ -7,10 +7,10 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, in
 	{
 		std::cout << "SDL init succes\n";
 		//init the window
-		m_pWindow = SDL_CreateWidow(title, xpos, ypos, width, height, flags);
+		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
 		if(m_pWindow != 0) // window init success
 		{
-			std::cout << "window creation success\n"
+			std::cout << "window creation success\n";
 		}
 	}
 }
