c703deff1126a29fbd4f281ebf1f3648fc51d448
Francisco Bahamondes <francisco.bahamondes@hipodromo.local>
2018-06-30 19:14:30 -0400

    books codes

--- SDL_01/Game.cpp
+++ SDL_01/Game.cpp
@@ -1,11 +1,12 @@
 #include "Game.h"
+#include<stdio.h>
 
 bool Game::init(const char* title, int xpos, int ypos, int width, int height, int flags)
 {
 	// attemp to initialize SDL
 	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
 	{
-		std::cout << "SDL init succes\n";
+		cout << "SDL init succes\n";
 		//init the window
 		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
 		if(m_pWindow != 0) // window init success
